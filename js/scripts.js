37fe22f42f04c19bafd196fd314189365ef392d8
BiancaPloss <94392113+biancajade95@users.noreply.github.com>
2023-08-27 11:59:26 -0400

    Added weather API

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,65 @@ const carousel = new bootstrap.Carousel('#homeCarousel', {
     pause: false
 })
 
-// when the pause button is clicked, pause the carousel
-const carouselPause = document.getElementById('carouselPause');
-carouselPause.addEventListener('click', function() {
-    console.log('pausing the carousel');
-    carousel.pause();
-})
+const carouselButton = document.getElementById('carouselButton');
+const faIcon = document.getElementById('faButton');
 
-// when the play button is clicked, begin cycling through the images
-const carouselPlay = document.getElementById('carouselPlay');
-carouselPlay.addEventListener('click', function() {
-    console.log('cycle the carousel');
-    carousel.cycle();
-})
+carouselButton.addEventListener('click', function () {
+    if (faIcon.classList.contains('fa-pause')) {
+        faIcon.classList.remove('fa-pause');
+        faIcon.classList.add('fa-play');
+        carousel.pause();
+    } else {
+        faIcon.classList.remove('fa-play');
+        faIcon.classList.add('fa-pause');
+        carousel.cycle();
+    }
+}) 
+   
+
+async function fetchWeather() {
+    const apiKey = process.env.OPEN_WEATHER_API_KEY;
+    const city = "Cleveland";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+    try {
+        const response = await fetch(url);
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayWeather(data);
+        } else {
+            console.error('API request was not successful:', data.message);
+        }
+    } catch (error) {
+        console.error('There was an error!', error);
+    }
+}
+
+(async () => {
+    await fetchWeather();
+   })();
+
+
+function displayWeather(weatherData) {
+    const temperature = weatherData.main.temp;
+    const description = weatherData.weather[0].description;
+    const icon = weatherData.weather[0].icon;
+
+    const iconUrl = `https://openweathermap.org/img/w/${icon}.png`;
+
+    const weatherIcon = document.createElement('img');
+    weatherIcon.src = iconUrl;
+
+    const temperatureElement = document.getElementById('weather-temp');
+    const descriptionElement = document.getElementById('weather-description');
+    const iconContainer = document.getElementById('weather-icon');
+
+    temperatureElement.textContent = `${temperature} \u00B0F`;
+    descriptionElement.textContent = `${description}`;
+
+    iconContainer.innerHTML = '';
+    iconContainer.appendChild(weatherIcon);
+}
+
